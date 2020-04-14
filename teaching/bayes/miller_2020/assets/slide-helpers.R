adf0db3fc83edfa6825778cb8ffe97c4982a5b4f
Michael DeCrescenzo <mgdecrescenzo@gmail.com>
2020-04-14 16:01:00 -0500

    Site build Tue Apr 14 16:00:56 CDT 2020

---
+++ teaching/bayes/miller_2020/assets/slide-helpers.R
@@ -0,0 +1,12 @@
+# ----------------------------------------------------
+#   Calls all slide helpers
+# ----------------------------------------------------
+
+# slim set of packages
+source(here::here("slides", "miller_2020", "assets", "slide-packages.R"))
+
+# contains colors
+source(here::here("slides", "miller_2020", "assets", "graphics-theme.R"))
+
+# chunk opts
+source(here::here("slides", "miller_2020", "assets", "slide-knit.R"))
---
+++ teaching/bayes/miller_2020/assets/slide-packages.R
@@ -0,0 +1,12 @@
+# Xaringan settings
+library("xaringan")
+library("xaringanthemer")
+
+library("here")
+library("magrittr")
+library("tidyverse")
+library("broom")
+
+library("rstan")
+library("brms")
+library("tidybayes")
